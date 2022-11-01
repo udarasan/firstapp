27d764d341befe6a6fb39a3fa50204d153f61ad9
udarasan <udarassanjeewa@gmail.com>
2022-11-02 00:28:42 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,79 @@
+import 'package:firstapp/screen/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
+
+
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+final List<Widget> screen=[
+  HomeScreen()
+];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home:Scaffold(
-        appBar: AppBar(
-          title: Text("To Do App"),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){},
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar:BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child:SizedBox(
+            height: 60,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.home,
+                  ),
+                  padding: EdgeInsets.only(left: 20.0),
+                  focusColor: Colors.cyan,
+                  onPressed: () {
+
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.group,
+                  ),
+                  padding: EdgeInsets.only(right: 20.0),
+                  focusColor: Colors.cyan,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.checklist,
+                  ),
+                  padding: EdgeInsets.only(left: 20.0),
+                  focusColor: Colors.cyan,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.person,
+                  ),
+                  padding: EdgeInsets.only(right: 20.0),
+                  focusColor: Colors.cyan,
+                  onPressed: () {},
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
-          selectedItemColor: Colors.blueAccent,
-          unselectedItemColor: Colors.lightBlue,
-        ),
+          )
+        )
       )
     );
   }
